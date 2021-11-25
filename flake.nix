25397fae51847941fc3e486bafb0d81fc92477e8
Rose Hudson <cv21874@bristol.ac.uk>
2021-11-25 11:06:55 +0000

    Add CircuitFlow to haskell shell

--- flake.nix
+++ flake.nix
@@ -1,12 +1,17 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.circuitflow = {
+    url = "github:rosehuds/CircuitFlow";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, circuitflow }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
       ghc-with-packages = pkgs.haskellPackages.ghcWithPackages (pkgs:
-        with pkgs; [ QuickCheck ]
+        [ pkgs.QuickCheck circuitflow.packages.${system}.CircuitFlow ]
       );
     in
     {
