25ac4adb3c5df2ceeda7f2ba864b1fb7dd512fd6
Rose Hudson <cv21874@bristol.ac.uk>
2022-11-09 22:26:26 +0000

    remove circuitflow from haskell shell

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.circuitflow = {
-    url = "github:rosehuds/CircuitFlow";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
-  };
 
-  outputs = { self, nixpkgs, flake-utils, circuitflow }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
       ghc-with-packages = pkgs.haskellPackages.ghcWithPackages (pkgs:
-        [ pkgs.QuickCheck circuitflow.packages.${system}.CircuitFlow ]
+        [ pkgs.QuickCheck ]
       );
       jdtls = pkgs.callPackage ./jdtls.nix {};
     in
